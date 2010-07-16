f37d03bee82c70838e51301741d74d35d0bec514
Duff OMelia <duff@omelia.org>
2010-07-16 12:53:20 -0400

    Use vim mode.

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -3,9 +3,12 @@
 # cat > /dev/null
 # And press it
 
+bindkey -v
+
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
 bindkey "^E"      end-of-line                          # ctrl-e
 bindkey "[B"      history-search-forward               # down arrow
 bindkey "[A"      history-search-backward              # up arrow
+
