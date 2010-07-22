83f83e705e6f545af66098294d06a6c31b513033
Duff OMelia <duff@omelia.org>
2010-07-22 17:53:22 -0400

    Fixed comment for bindkeys.

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -9,5 +9,5 @@ bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "∆"       history-search-forward               # down arrow mapped to Command-j
-bindkey "˚"       history-search-backward              # up arrow mapped to Command-k
+bindkey "∆"       history-search-forward               # down arrow mapped to Option-j
+bindkey "˚"       history-search-backward              # up arrow mapped to Option-k
