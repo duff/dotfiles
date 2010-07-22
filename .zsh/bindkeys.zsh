3d5f1f6487d0e5224a6257ea90499476b1f68b90
Duff OMelia <duff@omelia.org>
2010-07-22 17:51:13 -0400

    Mapped up arrow and down arrow to home row keys (vim's up/down)

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -9,6 +9,5 @@ bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "[B"      history-search-forward               # down arrow
-bindkey "[A"      history-search-backward              # up arrow
-
+bindkey "∆"       history-search-forward               # down arrow mapped to Command-j
+bindkey "˚"       history-search-backward              # up arrow mapped to Command-k
