5cbd209fd9beab3106974b829abaa05ea00d936d
Duff OMelia <duff@omelia.org>
2010-09-02 10:54:59 -0400

    Wasn't using the kill line bound key.
    Hooked up a keyboard shortcut for that in Terminal to scroll up.

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -5,7 +5,6 @@
 
 bindkey -v
 
-bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
 bindkey "^E"      end-of-line                          # ctrl-e
