49c3e7c071093245fbf26dae55335232f9928a83
Duff OMelia <duff@omelia.org>
2010-09-02 14:12:21 -0400

    Now have escape hooked up to caps-lock-l.
    I have caps-lock remapped to the option key.  This shortcut is now
    shared in vimium as well.  And regular vim has it there too in case
    muscle memory takes over.

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -10,4 +10,4 @@ bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
 bindkey "∆"       history-search-forward               # down arrow mapped to Option-j
 bindkey "˚"       history-search-backward              # up arrow mapped to Option-k
-bindkey "…"       vi-cmd-mode                          # option-; to go to normal mode w/o leaving home row
+bindkey "¬"       vi-cmd-mode                          # option-l to go to normal mode w/o leaving home row
