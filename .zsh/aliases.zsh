bd6c07dab09e25bf23cf97f08d81938dd1de4353
Duff OMelia <duff@omelia.org>
2012-02-25 15:08:21 -0500

    Remove that alias since there's now a hook to handle it automatically.
    Removing the alias ought to remove my `be` muscle memory.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -32,7 +32,6 @@ alias pow-debug-restart='touch tmp/debug.txt; pow-restart'
 # Bundler
 alias b='bundle'
 alias bi='bundle install --path vendor'
-alias be='bundle exec'
 
 # Misc
 alias listening_ports='lsof -i -n -P | grep LISTEN'   # Show which processes are using which ports
