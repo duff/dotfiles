cf6d22d4964bd1bb63251ff7d683d40864e9f200
Duff O'Melia <duff@omelia.org>
2016-10-05 13:14:22 -0400

    Fix keybindings

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -9,8 +9,8 @@ bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^F"      history-incremental-search-forward   # ctrl-f
 bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "∆"       history-search-forward               # down arrow mapped to Option-j
-bindkey "˚"       history-search-backward              # up arrow mapped to Option-k
+bindkey "^J"      history-search-forward               # down arrow mapped to ctrl-j
+bindkey "^K"      history-search-backward              # up arrow mapped to ctrl-k
 bindkey "¬"       vi-cmd-mode                          # maps to escape to go to normal mode Option-l
 bindkey ";;"      vi-cmd-mode                          # maps to escape to go to normal mode
 
