9040b2a79c8deac1a312109a94badc03bc746c1e
Duff OMelia <duff@omelia.org>
2019-06-06 10:51:40 -0400

    Stop overriding ls

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,6 @@
 
 alias g='git'
 
-# Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh'
-
 # Same as above, but in long listing format
 alias ll='ls -GFhl'
 
