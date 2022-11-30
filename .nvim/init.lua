9b4f96d2a91141f69ce2b765194d621424d34f73
Duff OMelia <duff@omelia.org>
2022-11-30 14:03:32 -0500

    Experiment with Neovim

---
+++ .nvim/init.lua
@@ -0,0 +1,15 @@
+
+vim.opt.number = true
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+
+vim.wo.fillchars='eob: '                 -- hide the ~ for blank lines
+
+-- Mappings
+vim.g.mapleader = ','
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
+vim.keymap.set('n', '<leader>c', ':sp $MYVIMRC<CR>')
+vim.keymap.set('n', '<leader>C', ':source $MYVIMRC<CR>:exe "echo \'init.lua reloaded\'"<CR>')
