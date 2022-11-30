d4b9271bd3ea8495c7675a39030f3fbe46dfd5dd
Duff OMelia <duff@omelia.org>
2022-11-30 15:18:49 -0500

    Update some Neovim options

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -1,10 +1,29 @@
 
-vim.opt.number = true
-vim.opt.hlsearch = false
-vim.opt.wrap = false
-vim.opt.tabstop = 4
+vim.o.number = true
+vim.o.hlsearch = false
+vim.o.wrap = false
 
-vim.wo.fillchars='eob: '                 -- hide the ~ for blank lines
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true                 -- expand tabs to spaces
+
+vim.o.scrolloff = 2                    -- keep some context when scrolling
+vim.o.sidescrolloff = 5                -- keep some context when scrolling side to side
+
+vim.o.fillchars='eob: '                -- hide the ~ for blank lines
+
+vim.o.backup = false                   -- don't keep backups after close
+vim.o.writebackup = false              -- don't keep backups while working
+vim.o.swapfile = false                 -- don't need swp files
+
+vim.o.ignorecase = true                -- make searces case insensitive
+vim.o.smartcase = true                 -- care about case sensitivity 
+
+vim.opt.completeopt = {'longest', 'menu', 'preview'}
+vim.opt.complete = {'.'}
+
+vim.opt.grepprg = 'rg --vimgrep --follow --color=never'
 
 -- Mappings
 vim.g.mapleader = ','
