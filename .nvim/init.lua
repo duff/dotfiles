29e92928d43cff3272f0115b6392c52931262749
Duff OMelia <duff@omelia.org>
2022-11-30 16:36:02 -0500

    Whitespace support Neovim

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -6,6 +6,7 @@ vim.o.wrap = false
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+
 vim.o.expandtab = true                 -- expand tabs to spaces
 
 vim.o.scrolloff = 2                    -- keep some context when scrolling
@@ -13,24 +14,36 @@ vim.o.sidescrolloff = 5                -- keep some context when scrolling side 
 
 vim.o.fillchars='eob: '                -- hide the ~ for blank lines
 
+
 vim.o.backup = false                   -- don't keep backups after close
 vim.o.writebackup = false              -- don't keep backups while working
 vim.o.swapfile = false                 -- don't need swp files
 
 vim.o.ignorecase = true                -- make searces case insensitive
-vim.o.smartcase = true                 -- care about case sensitivity 
+vim.o.smartcase = true                 -- care about case sensitivity
 
 vim.opt.completeopt = {'longest', 'menu', 'preview'}
 vim.opt.complete = {'.'}
 
 vim.opt.grepprg = 'rg --vimgrep --follow --color=never'
 
+vim.cmd('colorscheme base16-default-dark')
+
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'duff/vim-trailing-whitespace'
+end)
+
+
 -- Mappings
 vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>h', ':FixWhitespace<CR>')
 
 vim.keymap.set('n', '<leader>c', ':sp $MYVIMRC<CR>')
 vim.keymap.set('n', '<leader>C', ':source $MYVIMRC<CR>:exe "echo \'init.lua reloaded\'"<CR>')
 
-vim.cmd('colorscheme base16-default-dark')
+
